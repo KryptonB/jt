55039b6d448153061831e31bf9325b583a607c8f
Shamal <sratnappuli@virtusa.com>
2018-12-18 17:32:27 +0530

    Added triangle creation files

---
+++ Triangle.java
@@ -0,0 +1,26 @@
+public class Triangle {
+	public static void main(String[] args) {
+
+		int lines = 8;
+		int midPoint = lines;
+		int gridLength = (lines * 2) - 1;
+		int numberOfStars = 1;
+
+		for (int i = 1; i <= lines; i++) {
+			for (int j = 1; j <= midPoint; j++) {
+				if (j == midPoint) {
+					for (int k = 1; k <= numberOfStars; k++) {
+                        System.out.print('*');
+                    }			
+				} else {
+					System.out.print(' ');
+                    continue;
+				}
+				midPoint = midPoint - 1;
+				numberOfStars = numberOfStars + 2;
+			}
+            System.out.println(' ');
+		}
+
+	}
+}
---
+++ TriangleEmpty.java
@@ -0,0 +1,30 @@
+public class TriangleEmpty {
+	public static void main(String[] args) {
+
+		int lines = 8;
+		int midPoint = lines;
+		int gridLength = (lines * 2) - 1;
+		int numberOfStars = 1;
+
+		for (int i = 1; i <= lines; i++) {
+			for (int j = 1; j <= midPoint; j++) {
+				if (j == midPoint) {
+					for (int k = 1; k <= numberOfStars; k++) {
+                        if (k == 1 || k == numberOfStars) {
+                            System.out.print('*');
+                        } else {
+                            System.out.print(' ');
+                        }
+                    }
+				} else {
+					System.out.print(' ');
+                    continue;
+				}
+				midPoint = midPoint - 1;
+				numberOfStars = numberOfStars + 2;
+			}
+            System.out.println(' ');
+		}
+
+	}
+}
